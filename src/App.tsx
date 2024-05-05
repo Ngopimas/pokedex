262ae32a92a83475187556db7e4fb256fe55fb5e
Romain C. <49529524+Ngopimas@users.noreply.github.com>
2024-05-05 23:18:07 +0200

    add basic layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,35 @@
-import algoliasearch from 'algoliasearch/lite'
-import { InstantSearch } from 'react-instantsearch'
+import algoliasearch from "algoliasearch/lite";
+import { InstantSearch } from "react-instantsearch";
+import { APP_ID, INDEX_NAME, SEARCH_API_KEY } from "./lib/constants";
 
-const searchClient = algoliasearch('YOUR APP ID', 'YOUR SEARCH API KEY')
+const searchClient = algoliasearch(APP_ID, SEARCH_API_KEY);
 
 const App = () => {
   return (
     <>
-      <InstantSearch searchClient={searchClient} indexName="YOUR INDEX NAME">
-        <div className="flex flex-col justify-center items-center h-screen">
-          <div className="flex flex-col gap-2">
-            <h1 className="text-2xl font-bold text-center mb-8">
-              👋 Welcome to this assignement
-            </h1>
-            <ul>
-              <li>This boilerplate is providing:</li>
-              <li>- React</li>
-              <li>- Tailwind</li>
-              <li>- Typescript</li>
-              <li>- React Instant Search from Algolia</li>
-            </ul>
-            <p>Feel free to use it or build something from scratch</p>
-            <p>
-              Every information you need should be found in the
-              <a
-                target="_blank"
-                href="https://github.com/algolia/Demo-engineers-assignement"
-                className="text-indigo-800"
-              >
-                readMe
-              </a>
-            </p>
-            <p>
-              There is no time limit but you should aim around 4 hours to
-              complete it
-            </p>
-            <p>Happy coding 🥷</p>
+      <InstantSearch
+        future={{
+          preserveSharedStateOnUnmount: false,
+        }}
+        searchClient={searchClient}
+        indexName={INDEX_NAME}
+      >
+        <div className="grid overflow-hidden h-screen w-full md:grid-cols-[220px_1fr] lg:grid-cols-[280px_1fr]">
+          <div>Sidebar</div>
+
+          <div className="flex flex-col h-full max-h-screen gap-2">
+            <div>Header</div>
+            <main className="flex-1 px-4 overflow-auto">
+              <p>Content</p>
+            </main>
+            <div className="p-4 mt-auto border-t">
+              <div>Footer</div>
+            </div>
           </div>
         </div>
       </InstantSearch>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
---
+++ src/lib/constants.ts
@@ -0,0 +1,3 @@
+export const INDEX_NAME = import.meta.env.VITE_ALGOLIA_INDEX_NAME;
+export const APP_ID = import.meta.env.VITE_ALGOLIA_APP_ID;
+export const SEARCH_API_KEY = import.meta.env.VITE_ALGOLIA_SEARCH_API_KEY;
